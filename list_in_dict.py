97d8fbfd5a7945b02b06bb10f7704282a29c5bc8
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2022-03-28 21:19:23 +0530

    Add files via upload

---
+++ list_in_dict.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 21:04:39 2022
+
+@author: amrit
+"""
+
+ldict1 = {}
+
+ldict1["Name"] = ["Amrith"]
+ldict1["Age"] = [19]
+ldict1["College"] = ["Jain University"]
+
+print(ldict1)
+
+ldict2 = {}
+
+ldict2["Name"] = ["Kaushik"]
+ldict2["Age"] = [20]
+ldict2["College"] = ["Jain University"]
+
+print(ldict2)
+
+ldict3 = {}
+
+ldict3["Name"] = ["Surya"]
+ldict3["Age"] = [21]
+ldict3["College"] = ["Jain University"]
+
+print(ldict3)
+
+ldict4 = {}
+
+ldict4["Name"] = ["Allen"]
+ldict4["Age"] = [22]
+ldict4["College"] = ["Jain University"]
+
+print(ldict4)
